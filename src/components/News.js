35ce695dd86ec60567443df2931db66bad944d3d
Kanekiken-Aj <achintya.developer@gmail.com>
2023-10-09 01:26:00 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+
+export class News extends Component {
+ 
+
+  constructor() {
+    super();
+    // console.log("Hello I m A constructor form news component");
+    this.state = {
+      articles: [],
+      loading: false,
+      page:1,
+      
+    };
+  }
+
+  async componentDidMount(){
+    let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=e8fc83305d2c4605ac69e1575c4e8eec&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true})
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    // console.log(parsedData);
+    this.setState({articles: parsedData.articles, 
+      totalResults: parsedData.totalResults,
+      loading: false})
+  }
+
+  handlePrevClick = async ()=>{
+    console.log('prev clicked');
+    let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=e8fc83305d2c4605ac69e1575c4e8eec&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true})
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    // console.log(parsedData);
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false,
+    })
+  }
+
+  handleNextClick = async () =>{
+    
+
+    let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=e8fc83305d2c4605ac69e1575c4e8eec&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    // console.log(parsedData);
+
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles,
+      loading: false,
+    })
+  }
+
+
+  render() {
+    return (
+      <>
+      <div className="container my-3">
+        <h1 className="text-center"> NewsHunter - Top Headlines </h1>
+       {this.state.loading && <Spinner/>}
+        <div className="row">
+          
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-3" key={element.url}>
+                <NewsItem
+                  title={element.title}
+                  description={element.description}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" class="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" class="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+      </div>
+
+      </>
+    );
+  }
+}
+
+export default News;
