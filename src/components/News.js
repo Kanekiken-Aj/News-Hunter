1b4c270808303ab940ddf1e0fc69cf8bb3e964de
Kanekiken-Aj <achintya.developer@gmail.com>
2023-10-20 18:25:45 +0530

    author date and source added

--- src/components/News.js
+++ src/components/News.js
@@ -23,27 +23,36 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page:1
+      page:1,
       
-    }
+    };
   }
-  
+  // achintyakumarjaiswal api 9de2fa9b60034499914c74126278541a
   async componentDidMount(){
-    let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e8fc83305d2c4605ac69e1575c4e8eec&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    
-    // console.log(parsedData);
-    this.setState({
-      articles: parsedData.articles, 
-      totalResults: parsedData.totalResults,
-      loading: false})
+    try{
+        let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9de2fa9b60034499914c74126278541a&page=1&pageSize=${this.props.pageSize}`;
+        // let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e8fc83305d2c4605ac69e1575c4e8eec&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({loading: true})
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        
+        console.log(parsedData);
+        this.setState({
+          articles: parsedData.articles, 
+          totalResults: parsedData.totalResults,
+          loading: false,
+        });
+      }catch(error){
+        console.error("Error fetching data:", error);
+        this.setState({loading: false });
+      }
   }
 
   handlePrevClick = async ()=>{
     console.log('prev clicked');
-    let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e8fc83305d2c4605ac69e1575c4e8eec&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9de2fa9b60034499914c74126278541a&=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    
+    // let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e8fc83305d2c4605ac69e1575c4e8eec&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     this.setState({loading: true})
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -59,7 +68,9 @@ export class News extends Component {
   handleNextClick = async () =>{
     console.log("Next");
     if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-      let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e8fc83305d2c4605ac69e1575c4e8eec&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9de2fa9b60034499914c74126278541a&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      
+      // let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e8fc83305d2c4605ac69e1575c4e8eec&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
       this.setState({loading:true})
       let data = await fetch(url);
       let parsedData = await data.json()
@@ -89,6 +100,9 @@ export class News extends Component {
                   description={element.description?element.description:""}
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
+                  author={element.author}
+                  date = {element.publishedAt}
+                  source = {element.source.name}
                 />
               </div>
         
@@ -106,3 +120,15 @@ export class News extends Component {
 }
 
 export default News;
+
+
+
+
+
+
+
+
+
+
+
+// https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=9de2fa9b60034499914c74126278541a&page=1&pageSize=6
